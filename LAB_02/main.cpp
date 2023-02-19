5f0b97427bf13833b533d87a6e79579e98769f83
Alexandr <efimchiksasha@gmail.com>
2023-02-19 22:04:10 +0300

    add grvt_logger, run.bat and report screenshot. LAB_02 updated

--- LAB_02/main.cpp
+++ LAB_02/main.cpp
@@ -1,256 +1,228 @@
 #include <dos.h>
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-typedef struct {
-	unsigned char symb;
-	unsigned char color;
-} VIDEO;
+#define color 0x1D
 
-int color = 0x1F;
-
-void init();
-void read_registers();
-void decode_byte_to_bits(int indent, int byte);
-
-
-//System int (buffer)
-
-// IRQ0-7
-void _interrupt(*source_08) (...); // system timer
-void _interrupt(*source_09) (...); // keyboard controller
-void _interrupt(*source_0A) (...); // cascade (прерывание от slave контроллера)
-void _interrupt(*source_0B) (...); // serial port COM2
-void _interrupt(*source_0C) (...); // serial port COM1
-void _interrupt(*source_0D) (...); // parallel port 2 and 3 or sound card
-void _interrupt(*source_0E) (...); // floppy controller
-void _interrupt(*source_0F) (...); // parallel port 1
-
-// IRQ8-15
-void _interrupt(*source_70) (...); // CMOS
-void _interrupt(*source_71) (...); // ACPI
-void _interrupt(*source_72) (...); // open/SCSI/NIC
-void _interrupt(*source_73) (...); // open/SCSI/NIC
-void _interrupt(*source_74) (...); // mouse controller
-void _interrupt(*source_75) (...); // math co-processor
-void _interrupt(*source_76) (...); // ATA channel 1
-void _interrupt(*source_77) (...); // ATA channel 2
-
-
-// handled_ interrupt handlers
-// IRQ0-7
-void _interrupt handled_08(...) { read_registers(); source_08(); }
-void _interrupt handled_09(...) { read_registers(); source_09(); }
-void _interrupt handled_0A(...) { read_registers(); source_0A(); }
-void _interrupt handled_0B(...) { read_registers(); source_0B(); }
-void _interrupt handled_0C(...) { read_registers(); source_0C(); }
-void _interrupt handled_0D(...) { read_registers(); source_0D(); }
-void _interrupt handled_0E(...) { read_registers(); source_0E(); }
-void _interrupt handled_0F(...) { read_registers(); source_0F(); }
-
-// IRQ8-15
-void _interrupt handled_70(...) { read_registers(); source_70(); }
-void _interrupt handled_71(...) { read_registers(); source_71(); }
-void _interrupt handled_72(...) { read_registers(); source_72(); }
-void _interrupt handled_73(...) { read_registers(); source_73(); }
-void _interrupt handled_74(...) { read_registers(); source_74(); }
-void _interrupt handled_75(...) { read_registers(); source_75(); }
-void _interrupt handled_76(...) { read_registers(); source_76(); }
-void _interrupt handled_77(...) { read_registers(); source_77(); }
-
-void init()
+struct VIDEO
 {
-	// IRQ0-7
-	source_08 = getvect(0x08); //Timer
-	source_09 = getvect(0x09); //Keyboard
-	source_0A = getvect(0x0A); //SLAVE REQUEST
-	source_0B = getvect(0x0B); //COM2,4
-	source_0C = getvect(0x0C); //COM1,3
-	source_0D = getvect(0x0D); //LPT2
-	source_0E = getvect(0x0E); //Floppy
-	source_0F = getvect(0x0F); //LPT1
-	// IRQ8-15
-	source_70 = getvect(0x70); //CMOS
-	source_71 = getvect(0x71); //Respond of ray
-	source_72 = getvect(0x72); //Additional devices
-	source_73 = getvect(0x73); //Additional devices
-	source_74 = getvect(0x74); //Mouse
-	source_75 = getvect(0x75); //FPU
-	source_76 = getvect(0x76); //1 IDE-Controller
-	source_77 = getvect(0x77); //2 IDE-Controller
+	unsigned char symbol;
+	unsigned char attribute;
+};
 
-	setvect(0xD8, handled_08);
-	setvect(0xD9, handled_09);
-	setvect(0xDA, handled_0A);
-	setvect(0xDB, handled_0B);
-	setvect(0xDC, handled_0C);
-	setvect(0xDD, handled_0D);
-	setvect(0xDE, handled_0E);
-	setvect(0xDF, handled_0F);
+void print_byte(VIDEO far* screen, char byte)
+{
+	char temp;
+	for (char i = 0; i < 8; i++)
+	{
+		temp = byte % 2;
+		byte = byte >> 1;
+		screen->symbol = temp + '0';
+		screen->attribute = color;
+		screen++;
+	}
+}
 
-	setvect(0x08, handled_70);
-	setvect(0x09, handled_71);
-	setvect(0x0A, handled_72);
-	setvect(0x0B, handled_73);
-	setvect(0x0C, handled_74);
-	setvect(0x0D, handled_75);
-	setvect(0x0E, handled_76);
-	setvect(0x0F, handled_77);
+void print()
+{
+	char temp;
+	int i, val;
+	VIDEO far* screen = (VIDEO far *)MK_FP(0xB800, 0);
 
-	_disable(); // cli
+	val = inp(0x21);                // get mask Master registor
+	for (i = 0; i < 8; i++)
+	{
+		temp = val % 2;
+		val = val >> 1;
+		screen->symbol = temp + '0';
+		screen->attribute = color;
+		screen++;
+	}
+//	print_byte(screen, val);
+	screen++;
 
-	// Master
-	outp(0x20, 0x11);
-//	outp(0x21, 0xD8); // D8h basic vector
-	outp(0x21, 0x80	); // D8h basic vector
-	outp(0x21, 0x04);
-	outp(0x21, 0x01);
-	// Slave
-	outp(0xA0, 0x11);
-//	outp(0xA1, 0x08); // 08h basic vector
-	outp(0xA1, 0x08); // 08h basic vector
-	outp(0xA1, 0x02);
-	outp(0xA1, 0x01);
+	val = inp(0xA1);              // get mask Slave registor
+	for (i = 0; i < 8; i++)
+	{
+		temp = val % 2;
+		val = val >> 1;
+		screen->symbol = temp + '0';
+		screen->attribute = color;
+		screen++;
+	}
+//	print_byte(screen, val);
+	screen += 63;
+	outp(0x20,0x0A);
 
-	_enable(); // sti
+	val = inp(0x20);                    // get Master's request register
+	for (i = 0; i < 8; i++)
+	{
+		temp = val % 2;
+		val = val >> 1;
+		screen->symbol = temp + '0';
+		screen->attribute = color;
+		screen++;
+	}
+//	print_byte(screen, val);
+	screen++;
+
+	outp(0xA0,0x0A);
+	val = inp(0xA0);                     // get Slave's request register
+	for (i = 0; i < 8; i++)
+	{
+		temp = val % 2;
+		val = val >> 1;
+		screen->symbol = temp + '0';
+		screen->attribute = color;
+		screen++;
+	}
+//	print_byte(screen, val);
+	screen+=63;
+
+	outp(0x20,0x0B);
+	val = inp(0x20);                     // get Master's service register
+	for (i = 0; i < 8; i++)
+	{
+		temp = val % 2;
+		val = val >> 1;
+		screen->symbol = temp + '0';
+		screen->attribute = color;
+		screen++;
+	}
+//	print_byte(screen, val);
+	screen++;
+
+	outp(0xA0,0x0B);
+	val = inp(0xA0);                       // get Slave's service register
+	for (i = 0; i < 8; i++)
+	{
+		temp = val % 2;
+		val = val >> 1;
+		screen->symbol = temp + '0';
+		screen->attribute = color;
+		screen++;
+	}
+//	print_byte(screen, val);
+}
+
+// IRQ 0-7
+
+void interrupt(*base_interrupt_8) (...); 		// IRQ 0  - interrupt of timer (18,2 times per second)
+void interrupt(*base_interrupt_9) (...); 		// IRQ 1  - interrupt of keypad (press and release key)
+void interrupt(*base_interrupt_10) (...);		// IRQ 2  - interrupt for cascade interruptions in AT machines
+void interrupt(*base_interrupt_11) (...);		// IRQ 3  - interrupt of async port COM 2
+void interrupt(*base_interrupt_12) (...);		// IRQ 4  - interrupt of async port COM 1
+void interrupt(*base_interrupt_13) (...);		// IRQ 5  - interrupt of hard disk controller (for XT)
+void interrupt(*base_interrupt_14) (...);		// IRQ 6  - interrupt of floppy disk controller (when finish operation with floppy disk)
+void interrupt(*base_interrupt_15) (...);		// IRQ 7  - interrupt of printer (when printer is ready to work)
+
+// IRQ 8-15
+
+void interrupt(*base_interrupt_70) (...);		// IRQ 8  - interrupt of real time clock
+void interrupt(*base_interrupt_71) (...);		// IRQ 9  - interrupt of EGA controller
+void interrupt(*base_interrupt_72) (...);		// IRQ 10 - reserved interrupt
+void interrupt(*base_interrupt_73) (...);		// IRQ 11 - reserved interrupt
+void interrupt(*base_interrupt_74) (...);		// IRQ 12 - reserved interrupt
+void interrupt(*base_interrupt_75) (...);		// IRQ 13 - interrupt of mathematic soprocessor
+void interrupt(*base_interrupt_76) (...);		// IRQ 14 - interrupt of hard disk
+void interrupt(*base_interrupt_77) (...);		// IRQ 15 - reserved interrupt
+
+
+void interrupt  hooked_interrupt08(...) { print(); base_interrupt_8(); }
+void interrupt  hooked_interrupt09(...) { print(); base_interrupt_9(); }
+void interrupt  hooked_interrupt0A(...) { print(); base_interrupt_10(); }
+void interrupt  hooked_interrupt0B(...) { print(); base_interrupt_11(); }
+void interrupt  hooked_interrupt0C(...) { print(); base_interrupt_12(); }
+void interrupt  hooked_interrupt0D(...) { print(); base_interrupt_13(); }
+void interrupt  hooked_interrupt0E(...) { print(); base_interrupt_14(); }
+void interrupt  hooked_interrupt0F(...) { print(); base_interrupt_15(); }
+
+void interrupt  hooked_interruptC0(...) { print(); base_interrupt_70(); }
+void interrupt  hooked_interruptC1(...) { print(); base_interrupt_71(); }
+void interrupt  hooked_interruptC2(...) { print(); base_interrupt_72(); }
+void interrupt  hooked_interruptC3(...) { print(); base_interrupt_73(); }
+void interrupt  hooked_interruptC4(...) { print(); base_interrupt_74(); }
+void interrupt  hooked_interruptC5(...) { print(); base_interrupt_75(); }
+void interrupt  hooked_interruptC6(...) { print(); base_interrupt_76(); }
+void interrupt  hooked_interruptC7(...) { print(); base_interrupt_77(); }
+
+void initialize()
+{
+	base_interrupt_8 = getvect(0x08);
+	base_interrupt_9 = getvect(0x09);
+	base_interrupt_10 = getvect(0x0A);
+	base_interrupt_11 = getvect(0x0B);
+	base_interrupt_12 = getvect(0x0C);
+	base_interrupt_13 = getvect(0x0D);
+	base_interrupt_14 = getvect(0x0E);
+	base_interrupt_15 = getvect(0x0F);
+
+
+	base_interrupt_70 = getvect(0x70);
+	base_interrupt_71 = getvect(0x71);
+	base_interrupt_72 = getvect(0x72);
+	base_interrupt_73 = getvect(0x73);
+	base_interrupt_74 = getvect(0x74);
+	base_interrupt_75 = getvect(0x75);
+	base_interrupt_76 = getvect(0x76);
+	base_interrupt_77 = getvect(0x77);
+
+	//set new handlers for IRQ 0-7
+	setvect(0x80, hooked_interrupt08);
+	setvect(0x81, hooked_interrupt09);
+	setvect(0x82, hooked_interrupt0A);
+	setvect(0x83, hooked_interrupt0B);
+	setvect(0x84, hooked_interrupt0C);
+	setvect(0x85, hooked_interrupt0D);
+	setvect(0x86, hooked_interrupt0E);
+	setvect(0x87, hooked_interrupt0F);
+
+	//set new handlers for IRQ8-15
+	setvect(0x08, hooked_interruptC0);
+	setvect(0x09, hooked_interruptC1);
+	setvect(0x0A, hooked_interruptC2);
+	setvect(0x0B, hooked_interruptC3);
+	setvect(0x0C, hooked_interruptC4);
+	setvect(0x0D, hooked_interruptC5);
+	setvect(0x0E, hooked_interruptC6);
+	setvect(0x0F, hooked_interruptC7);
+
+	_disable(); // CLI
+
+	// interrupt init for Master
+	outp(0x20, 0x11);	//ICW1 - initialize master
+	outp(0x21, 0x80);   //ICW2 - base vector for master
+	outp(0x21, 0x04);	//ICW3 - the port bit of Slave (in binary format)
+	outp(0x21, 0x01);	//ICW4 - default
+
+	// interrupt init for Slave
+	outp(0xA0, 0x11);  //ICW1 - initialize Slave
+	outp(0xA1, 0x08);  //ICW2 - base vector for slave
+	outp(0xA1, 0x02);  //ICW3 - the port number of connected port on Master
+	outp(0xA1, 0x01);  //ICW4 - default
+
+	_enable(); // STI
 }
 
 int main()
 {
-	init();
-	// Make resident program
-	unsigned far* fp;
-	FP_SEG(fp) = _psp;
-	FP_OFF(fp) = 0x2c;
-	_dos_freemem(*fp);
-	_dos_keep(0, (_DS - _CS) + (_SP / 16) + 1);
+	unsigned far *fp;
+	initialize();
+	system("cls");
+
+	printf("                   - mask\n");
+	printf("                   - prepare\n");
+	printf("                   - service\n");
+	printf("Master  | Slave\n");
+
+	FP_SEG(fp) = _psp; //получаем сегмент
+	FP_OFF(fp) = 0x2c; //получаем смещение сегмента данных с переменными среды
+	_dos_freemem(*fp); //освобождение памяти под них
+
+	_dos_keep(0, (_DS - _CS) + (_SP / 16) + 1); //оставляем резидентной, первый параметр – код
+												//завершения, второй – объём памяти, который должен быть зарезервирован
+												//для программы после её завершения
+
 	return 0;
-}
-
-// Read registers
-void read_registers()
-{
-	int gui_offset = 40;
-	int reg;
-
-	VIDEO far* screen = (VIDEO far*)MK_FP(0xB800, 0);
-
-	screen += gui_offset;
-	screen->symb = 'I';
-	screen->color = color;
-	screen++;
-	screen->symb = 'M';
-	screen->color = color;
-	screen++;
-	screen->symb = 'R';
-	screen->color = color;
-
-	screen += 7;
-	screen->symb = 'I';
-	screen->color = color;
-	screen++;
-	screen->symb = 'R';
-	screen->color = color;
-	screen++;
-	screen->symb = 'R';
-	screen->color = color;
-
-	screen += 7;
-	screen->symb = 'I';
-	screen->color = color;
-	screen++;
-	screen->symb = 'S';
-	screen->color = color;
-	screen++;
-	screen->symb = 'R';
-	screen->color = color;
-
-	screen -= gui_offset + 20;
-
-	screen += 80 + gui_offset - 10;
-	screen->symb = 'M';
-	screen->color = color;
-	screen++;
-	screen->symb = 'A';
-	screen->color = color;
-	screen++;
-	screen->symb = 'S';
-	screen->color = color;
-	screen++;
-	screen->symb = 'T';
-	screen->color = color;
-	screen++;
-	screen->symb = 'E';
-	screen->color = color;
-	screen++;
-	screen->symb = 'R';
-	screen->color = color;
-	screen++;
-	screen->symb = ':';
-	screen->color = color;
-
-	screen -= gui_offset - 10 + 80 + 6;
-
-
-	screen += 80 * 2 + gui_offset - 10;
-	screen->symb = 'S';
-	screen->color = color;
-	screen++;
-	screen->symb = 'L';
-	screen->color = color;
-	screen++;
-	screen->symb = 'A';
-	screen->color = color;
-	screen++;
-	screen->symb = 'V';
-	screen->color = color;
-	screen++;
-	screen->symb = 'E';
-	screen->color = color;
-	screen++;
-	screen->symb = ':';
-	screen->color = color;
-
-
-
-
-	// Leading controller
-	reg = inp(0x21); // IMR - mask
-	decode_byte_to_bits(80+0+gui_offset, reg);
-
-	outp(0x20, 0x0B); // IRR - request
-	reg = inp(0x20);
-	decode_byte_to_bits(80+9+gui_offset, reg);
-
-	outp(0x20, 0x0A); // ISR - service
-
-	reg = inp(0x20);
-	decode_byte_to_bits(80+18+gui_offset, reg);
-
-	reg = inp(0xA1); // IMR
-	decode_byte_to_bits(80*2+gui_offset, reg);
-
-	outp(0xA0, 0x0B); //IRR
-	reg = inp(0xA0);
-	decode_byte_to_bits(80*2+9+gui_offset, reg);
-
-	outp(0xA0, 0x0A); // ISR
-	reg = inp(0xA0);
-	decode_byte_to_bits(80*2+18+gui_offset, reg);
-}
-
-void decode_byte_to_bits(int indent, int byte)
-{
-	char bit;
-	int i;
-	VIDEO far* screen = (VIDEO far*)MK_FP(0xB800, 0);
-	screen += indent;
-	for (i = 7; i >= 0; i--)
-	{
-		//Count bits
-		bit = byte % 2;
-		byte /= 2;
-		//Fill the screen
-		screen->symb = bit + '0';
-		screen->color = color;
-		screen++;
-	}
 }
