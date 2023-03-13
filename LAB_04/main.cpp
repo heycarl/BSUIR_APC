dbd4f86cbb829806787588e207fcb0cffa479265
Alexandr <efimchiksasha@gmail.com>
2023-03-13 21:59:27 +0300

    add RTC registers defines, changing frequency implemented

--- LAB_04/main.cpp
+++ LAB_04/main.cpp
@@ -11,36 +11,125 @@ int ms;
 #define CMOS_TIME_COUNT 6
 
 char data[CMOS_TIME_COUNT];
-// function pointers for system interrupt handlers (timer and alarm)
 void interrupt (*__timer_handler_sys)(...);
-//void interrupt (*__alarm_handler_sys)(...);
+
 unsigned char convert_to_dec(int value);
 unsigned char convert_to_bcd(int value);
+
+#define RTC_CURRENT_SEC 0x00
+//00h: RTC — текущая секунда (00 – 59h или 00 – 3Bh) — формат выбирается регистром 0Bh, по умолчанию — BCD
+
+#define RTC_CURRENT_ALARM_SEC 0x01
+//01h: RTC — секунды будильника (00 – 59h или 00 – 3Bh или FFh (любая секунда))
+
+#define RTC_CURRENT_MIN 0x02
+//02h: RTC — текущая минута (00 – 59h или 00 – 3Bh)
+
+#define RTC_CURRENT_ALARM_MIN 0x03
+//03h: RTC — минуты будильника (00 – 59h или 00 – 3Bh или FFh)
+
+#define RTC_CURRENT_HOUR 0x04
+//04h: RTC — текущий час:
+//		0 – 23h/00 – 17h (24-часовой режим)
+//		1 – 12h/01 – 1Ch (12-часовой режим до полудня)
+//		81h – 92h/81 – 8Ch (12-часовой режим после полудня)
+
+#define RTC_CURRENT_ALARM_HOUR 0x05
+//05h: RTC — часы будильника (то же или FFh, если любой час)
+
+#define RTC_CURRENT_WEEK_DAY 0x06
+//06h: RTC — текущий день недели (1 – 7, 1 — воскресенье)
+
+#define RTC_CURRENT_MONTH_DAY 0x07
+//07h: RTC — текущий день месяца (01 – 31h/01h – 1Fh)
+
+#define RTC_CURRENT_MONTH 0x08
+//08h: RTC — текущий месяц (01 – 12h/01 – 0Ch)
+
+#define RTC_CURRENT_YEAR 0x09
+//09h: RTC — текущий год (00 – 99h/00 – 63h)
+
+#define RTC_STATE_A 0x0A
+//0Ah: RTC — регистр состояния А
+//		бит 7: 1 — часы заняты (происходит обновление)
+//		биты 4 – 6: делитель фазы (010 — 32 768 KHz — по умолчанию)
+//		биты 3 – 0: выбор частоты периодического прерывания:
+//			0000 — выключено
+//			0011 — 122 микросекунды (минимум)
+//			1111 — 500 миллисекунд
+//			0110 — 976,562 микросекунды (1024 Hz)
+
+#define RTC_STATE_B 0x0b
+//0Bh: RTC — регистр состояния В
+//		бит 7: запрещено обновление часов (устанавливают перед записью новых значений в регистры даты и часов)
+//		бит 6: вызов периодического прерывания (IRQ8)
+//		бит 5: вызов прерывания при срабатывании будильника
+//		бит 4: вызов прерывания по окончании обновления времени
+//		бит 3: включена генерация прямоугольных импульсов
+//		бит 2: 1/0 — формат даты и времени двоичный/BCD
+//		бит 1: 1/0 — 24-часовой/12-часовой режим
+//		бит 0: автоматический переход на летнее время в апреле и октябре
+
+#define RTC_STATE_C 0x0c
+//0Ch только для чтения: RTC — регистр состояния С
+//		бит 7: произошло прерывание
+//		бит 6: разрешено периодическое прерывание
+//		бит 5: разрешено прерывание от будильника
+//		бит 4: разрешено прерывание по окончании обновления часов
+
+//0Dh только для чтения: регистр состояния D
+//		бит 7: питание RTC/CMOS есть
+
+
 void refresh_time();
 void print_time();
 void set_time();
 void make_delay(unsigned int ms);
-//void set_alarm();
 void enter_new_time();
+void change_freq();
 
-//void interrupt __new_alarm_handler(...)
-//{
-//	outp(0x70, 0x0C);
-//	if (inp(0x71) & 0x20) {
-//		puts("ALARM!!!\n");
-//	}
-//	outp(0x20, 0x20);
-//	outp(0x0A, 0x20);
-//	disable(); // CLI
-//	setvect(0x70, __alarm_handler_sys);
-//	enable(); // STI
-//	outp(0x70, 0x0B);
-//	outp(0x71, (inp(0x71) & 0xDF));
-//}
+void change_freq(){
+	int freq;
+	int q;
+	int bin;
+	puts("select frequency: ");
+	puts("1 2 hertz");
+	puts("2 4 hertz");
+	puts("3 8 hertz");
+	puts("4 16 hertz");
+	puts("5 32 hertz");
+	puts("6 64 hertz");
+	puts("7 128 hertz");
+	puts("8 256 hertz");
+	puts("9 512 hertz");
+	puts("10 1024 hertz");
+	puts("11 2048 hertz");
+	puts("12 4096 hertz");
+	puts("13 8192 hertz");
+	scanf("%d", &q);
+	freq = 0x0F - q + 1;
+
+	outp(0x70, RTC_STATE_A);
+	bin = inp(0x71);
+	printf("frequency set before %X \n", bin);
+
+	outp(0x70, RTC_STATE_A);
+	outp(0x71, inp(0x71) | 0x80); // 1000 0000 (forbid to change)
+
+	outp(0x70, RTC_STATE_A);
+	outp(0x71, inp(0x71) & (0x20 + freq)); // 0010 0000 + freq
+
+	outp(0x70, RTC_STATE_A);
+	outp(0x71, inp(0x71) & 0x7f); // 0111 1111 (allow to change)
+
+	outp(0x70, RTC_STATE_A);
+	bin = inp(0x71);  //00100110
+	printf("\nchanged frequency %X \n", bin);
+}
 
 void interrupt __new_timer_handler(...)
 {
-	outp(0x70, 0x0C);
+	outp(0x70, RTC_STATE_C);
 	if (inp(0x71) & 0x40) {
 		delay_time++;
 	}
@@ -52,7 +141,7 @@ void interrupt __new_timer_handler(...)
 		disable(); // CLI
 		setvect(0x70, __timer_handler_sys);
 		enable(); // STI
-		outp(0x70, 0x0B);
+		outp(0x70, RTC_STATE_B);
 		outp(0x71, (inp(0x71) & 0xBF));
 	}
 }
@@ -70,11 +159,11 @@ int main()
 {
 	while (1)
 	{
-		printf("1 Current time\n");
-		printf("2 Set time\n");
-//		printf("3 Set alarm\n");
-		printf("3 Set delay\n");
-		printf("4 Exit\n");
+		printf("1 current time\n");
+		printf("2 set time\n");
+		printf("3 set delay\n");
+		printf("4 change freq\n");
+		printf("5 exit\n");
 		switch (getch())
 		{
 			case '1':
@@ -86,10 +175,6 @@ int main()
 				system("cls");
 				set_time();
 				break;
-//			case '3':
-//				system("cls");
-//				set_alarm();
-//				break;
 			case '3':
 				system("cls");
 				fflush(stdin);
@@ -98,6 +183,9 @@ int main()
 				make_delay(ms);
 				break;
 			case '4':
+				change_freq();
+				break;
+			case '5':
 				system("cls");
 				return 0;
 			default:
@@ -110,7 +198,13 @@ int main()
 
 void refresh_time()
 {
-	unsigned int time_cmos[] = { 0x00, 0x02, 0x04, 0x07, 0x08, 0x09 };
+	unsigned int time_cmos[] = { RTC_CURRENT_SEC,
+								 RTC_CURRENT_MIN,
+								 RTC_CURRENT_HOUR,
+								 RTC_CURRENT_MONTH_DAY,
+								 RTC_CURRENT_MONTH,
+								 RTC_CURRENT_YEAR
+	};
 	for (int i = 0; i < CMOS_TIME_COUNT; i++) {
 		outp(0x70, time_cmos[i]);
 		data[i] = inp(0x71);
@@ -151,7 +245,7 @@ void set_time()
 {
 	enter_new_time();
 	disable(); // CLI
-	outp(0x70, 0x0B);
+	outp(0x70, RTC_STATE_B);
 //	бит 7: запрещено обновление часов (устанавливают перед записью новых значений в регистры даты и часов)
 //	бит 6: вызов периодического прерывания (IRQ8)
 //	бит 5: вызов прерывания при срабатывании будильника
@@ -161,26 +255,15 @@ void set_time()
 //	бит 1: 1/0 — 24-часовой/12-часовой режим
 //	бит 0: автоматический переход на летнее время в апреле и октябре
 
-	outp(0x71, inp(0x71) & 0x7F); // запретить обновление часов
-	int time_cmos[] = { 0x00, 0x02, 0x04 };
-//	00h: RTC — текущая секунда (00 – 59h или 00 – 3Bh) — формат выбирается регистром 0Bh, по умолчанию — BCD
-//	01h: RTC — секунды будильника (00 – 59h или 00 – 3Bh или FFh (любая секунда))
-//	02h: RTC — текущая минута (00 – 59h или 00 – 3Bh)
-//	03h: RTC — минуты будильника (00 – 59h или 00 – 3Bh или FFh)
-//	04h: RTC — текущий час:
-//		0 – 23h/00 – 17h (24-часовой режим)
-//		1 – 12h/01 – 1Ch (12-часовой режим до полудня)
-//	81h – 92h/81 – 8Ch (12-часовой режим после полудня)
-//	05h: RTC — часы будильника (то же или FFh, если любой час)
-//	06h: RTC — текущий день недели (1 – 7, 1 — воскресенье)
-//	07h: RTC — текущий день месяца (01 – 31h/01h – 1Fh)
-//	08h: RTC — текущий месяц (01 – 12h/01 – 0Ch)
-//	09h: RTC — текущий год (00 – 99h/00 – 63h)
+	outp(0x71, inp(0x71) & 0x7f); // запретить обновление часов
+	int time_cmos[] = { RTC_CURRENT_SEC, RTC_CURRENT_MIN, RTC_CURRENT_HOUR };
 
 	for (int i = 0; i < 3; i++) {
 		outp(0x70, time_cmos[i]);
 		outp(0x71, data[i]);
 	}
+	
+	outp(0x71, inp(0x71) | 0x80); // разрешить обновление часов
 	enable(); // STI
 }
 
@@ -193,27 +276,9 @@ void make_delay(unsigned int ms)
 	setvect(0x70, __new_timer_handler);
 	outp(0xA1, (inp(0xA0) & 0xFE));
 	enable(); // STI
-	outp(0x70, 0x0B);
+	outp(0x70, RTC_STATE_B);
 	outp(0x71, (inp(0x71) | 0x40));
 }
-//
-//void set_alarm()
-//{
-//	enter_new_time();
-//	disable(); // CLI
-//	__alarm_handler_sys = getvect(0x70);
-//	setvect(0x70, __new_alarm_handler);
-//	outp(0xA1, (inp(0xA0) & 0xFE));
-//	int alarm_cmos[] = { 0x01, 0x03, 0x05 };
-//	for (int i = 0; i < 3; i++) {
-//		outp(0x70, alarm_cmos[i]);
-//		outp(0x71, data[i]);
-//	}
-//	enable(); // STI
-//	outp(0x70, 0x0B);
-//	outp(0x71, inp(0x71) | 0x20);
-//	puts("Alarm enabled!\n");
-//}
 
 int get_value_in_range(int low, int high, char* const instruct)
 {
